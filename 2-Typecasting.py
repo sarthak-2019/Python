2645d05c11ca6a5d7f780fa8ea16e78b1008a2c0
Sarthak <sarthak2019fractal@gmail.com>
2025-01-03 12:03:15 +0530

    L2

---
+++ 2-Typecasting.py
@@ -0,0 +1,25 @@
+# Explicit TypeCasting
+
+a="1"
+b="2"
+
+print(a+b)
+
+
+# int()
+# float()
+# str()
+# ord()
+# tuple()
+# set()
+# list()
+# dict()
+
+
+
+# Implicit Typecasting
+
+c=1.9
+d=9
+
+print(c+d)
