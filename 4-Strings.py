5c10d3a67f919b3e55f4a14a841460102604b91b
Sarthak <sarthak2019fractal@gmail.com>
2025-01-03 22:55:22 +0530

    L4&L5

---
+++ 4-Strings.py
@@ -0,0 +1,18 @@
+# Multi line String
+
+
+str=''' Hello Sarthak
+I am good 
+Happy to see u
+'''
+
+
+print(str)
+
+
+
+# For loop in string
+
+
+for char in str:
+    print(char)
---
+++ 5-String_Slicing.py
@@ -0,0 +1,21 @@
+names="Sarthak Shukla"
+
+
+# To slice the string (index:length we are slicing)
+print(names[0:5])
+
+# Automaticlly sets 0 index ([0:5])
+print(names[:5])
+
+# To get Length of the String
+print(len(names))
+
+
+# Negative Slicing
+# python consider it as [0: len(names)-2]
+print(names[0:-2])
+
+# We can also start negiative for staring index tooo
+str="Harry"
+print(str[-4:-2])
+
