2f1bede38f549b50a1a9b96c609a66153ec53d23
Sarthak <sarthak2019fractal@gmail.com>
2025-01-03 11:04:26 +0530

    L1

---
+++ 1-Variables_DataTypes.py
@@ -0,0 +1,27 @@
+# 1-Numeric Data Type
+# int,float,complex
+
+
+# 2-Text Data str
+# str
+
+
+# 3-Boolean Data
+
+
+# 4- Sequenced Data [list,tuple]
+# List-Mutable 
+# Tuple-Immutable
+
+# 5- Dictionary [Key value Pair]
+
+a=complex(8,2)
+b=True
+c="Sarthak"
+d=None
+
+print("Type of a",type(a))
+print("Type of b",type(b))
+print("Type of c",type(c))
+print("Type of d",type(d))
+
