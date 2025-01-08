14fb6b252c9bc90a1d474cf660514f8c305ec7b3
Sarthak <sarthak2019fractal@gmail.com>
2025-01-08 12:13:35 +0530

    L9&L10

---
+++ 10-While_Loops.py
@@ -0,0 +1,14 @@
+
+i=0
+
+while(i<3):
+    print(i)
+    i=i+1
+
+
+while(i<=38):
+    i=int(input("Enter Value of i: "))
+    print(i)
+
+print("Done with Loop")
+
---
+++ 9-For_Loops.py
@@ -0,0 +1,12 @@
+name="sarthak"
+
+
+for i in name:
+    print(i)
+
+for k in range(5):
+    print(k+1)
+
+for k in range(1,12,3):
+    print(k)
+
