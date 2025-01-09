f4ac8ede133916fc271015b50cb5f27adc3ff389
Sarthak <sarthak2019fractal@gmail.com>
2025-01-09 20:37:41 +0530

    L15&L16

---
+++ 15-f-strings.py
@@ -0,0 +1,10 @@
+name="Sarthak"
+country="India"
+print(f"Hi my name is {name} and I am from {country}")
+
+
+# Format the number
+
+price=49.0999
+
+print(f"Formatted price is {price:.2f} dollars")
---
+++ 16-DocStrings.py
@@ -0,0 +1,17 @@
+#Can be asked in Interview 
+
+# Doc strings appear right after
+# definition of function,method,
+# class or module
+
+# Doc strings is right below the function name
+
+def square(n):
+    ''' Takes number n, returns the square n'''
+    print(n**2)
+
+square(5)
+print(square.__doc__)
+
+
+
