46f4372fd6a5199b74e537a3de4f442b1b79ab09
Sarthak <sarthak2019fractal@gmail.com>
2025-01-19 21:52:59 +0530

    l25

---
+++ 25-OsModule.py
@@ -0,0 +1,15 @@
+import os
+
+
+if(not os.path.exists("data")):
+    os.mkdir("data")
+
+for i in range(0,100):
+    os.mkdir(f"data/Day{i+1}")
+
+# for i in range(0,100):
+#     os.rename(f"data/Day{i+1}",f"data/DayDate{i+1}")
+
+# folders=os.listdir("data")
+
+# print(folders)
